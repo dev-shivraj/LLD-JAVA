56e8b992ba3b1f2dec4b131f1fd8504b3f658f2f
kumar-shivraj <dev.shivraj.kumar@gmail.com>
2026-08-03 10:40:05 +0530

    concurrency: visualising how multiple thread using future saves the time if tasks are done in parallel

--- concurrency/src/multithreading/callablesandfutures/Client.java
+++ concurrency/src/multithreading/callablesandfutures/Client.java
@@ -8,16 +8,23 @@ import java.util.concurrent.Future;
 public class Client {
     static void main() throws ExecutionException, InterruptedException {
         ExecutorService ex = Executors.newFixedThreadPool(2);
-        NumberMultiplier numberMultiplier = new NumberMultiplier(5);
+        NumberMultiplier numberMultiplier1 = new NumberMultiplier(5);
+        NumberMultiplier numberMultiplier2 = new NumberMultiplier(10);
 
         // execute => when we don't need to return the result from the thread
         // submit => when we need to return the result from the thread
-        Future<Integer> future = ex.submit(numberMultiplier);
+        Future<Integer> future1 = ex.submit(numberMultiplier1);
+        Future<Integer> future2 = ex.submit(numberMultiplier2);
 
         System.out.println("Hello from main thread");
 
-        int res = future.get(); // This will block until the result is available
-        System.out.println("Result from thread: " + res);
+        int res1 = future1.get(); // This will block until the result is available
+        int res2 = future2.get(); // This will block until the result is available
+
+        // if thread1 takes 3 sec to complete and thread2 takes 3 sec to complete
+        // then the total time taken will be 3 sec because both threads are running in parallel
+        System.out.println("Result from thread 1: " + res1);
+        System.out.println("Result from thread 2: " + res2);
 
         System.out.println("Hello from main thread after getting the result");
 
