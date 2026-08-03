4277f530d2193babc446b8dce0e56fa8b811bd08
kumar-shivraj <dev.shivraj.kumar@gmail.com>
2026-08-03 16:19:52 +0530

    concurrency: merge sort use cached thread pool instead of fixed thread pool and also use shutdown

--- concurrency/src/multithreading/mergesortusingmultithreading/Client.java
+++ concurrency/src/multithreading/mergesortusingmultithreading/Client.java
@@ -11,11 +11,14 @@ public class Client {
         // Create a list of integers to sort
         List<Integer> list = Arrays.asList(38, 27, 43, 3, 9, 82, 10);
 
-        ExecutorService ex = Executors.newFixedThreadPool(list.size());
+//        ExecutorService ex = Executors.newFixedThreadPool(list.size());
+        ExecutorService ex = Executors.newCachedThreadPool();
         Sorter sorter = new Sorter(list, ex);
 
         Future<List<Integer>> future = ex.submit(sorter);
         List<Integer> result = future.get();
         System.out.println("Sorted list: " + result);
+
+        ex.shutdown();
     }
 }
