3fb05328983bc4d26319d057a4bf199182ae8666
kumar-shivraj <dev.shivraj.kumar@gmail.com>
2026-08-05 13:27:51 +0530

    synchronization: synchronized debug

--- concurrency/src/synchronization/synchronizedkeyword/Adder.java
+++ concurrency/src/synchronization/synchronizedkeyword/Adder.java
@@ -9,13 +9,18 @@ public class Adder implements Callable<Void> {
         this.value = value;
     }
 
-    public void add() {
-        for (int i = 1; i <= 100000; i++) {
+    public void add() throws InterruptedException {
+        for (int i = 1; i <= 100; i++) {
             synchronized (this.value) {
-                System.out.println("Entering synchronized block in Adder, current value: " + this.value.getX());
+                System.out.println("=======================================================================================");
+                System.out.println("Entered synchronized block in Adder, current value: " + this.value.getX());
                 this.value.setX(this.value.getX() + 1);
+
+                // Simulate some processing time to increase the chance of context switching and thread contention
+                Thread.sleep(1);
                 System.out.println("Thread " + Thread.currentThread().getName() + " added 1, new value: " + this.value.getX());
                 System.out.println("Exiting synchronized block in Adder, current value: " + this.value.getX());
+                System.out.println("=======================================================================================");
             }
         }
     }
