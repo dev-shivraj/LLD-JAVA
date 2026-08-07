dcc5ad75fbe694dd30caebbfaf7ef157f38adb68
kumar-shivraj <dev.shivraj.kumar@gmail.com>
2026-08-07 09:30:32 +0530

    synchronization/semaphore: resolve producer consumer problem using synchronized method in store

--- concurrency/src/synchronization/semaphore/producerconsumerproblem/Store.java
+++ concurrency/src/synchronization/semaphore/producerconsumerproblem/Store.java
@@ -8,14 +8,14 @@ public class Store {
     private List<Object> items;
 
     // ======================== methods ==============================
-    public void addItem(Object item) {
+    public synchronized void addItem(Object item) {
         if(items.size() < maxSize) {
             items.add(item);
             System.out.println("Produced an item. Total items: " + items.size());
         }
     }
 
-    public void removeItem() {
+    public synchronized void removeItem() {
         if(items.size() > 0) {
             items.remove(items.size() - 1);
             System.out.println("Consumed an item. Total items: " + items.size());
