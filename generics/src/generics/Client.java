16e6eec9b032cb4834123c523534c1eb3cf2dca1
kumar-shivraj <dev.shivraj.kumar@gmail.com>
2026-08-10 03:51:08 +0530

    generics: issue with raw/Object type

--- generics/src/generics/Client.java
+++ generics/src/generics/Client.java
@@ -8,6 +8,11 @@ public class Client {
         p.setX(10);
         p.setY(20);
 
+        // this will cause problem as we are trying to set a String value to a Pair that is supposed to hold integers
+        // at compile time, this will not cause any error as we are using raw types
+        // but at runtime, it will throw a ClassCastException
+        p.setX("Hello");
+
         int x = (int) p.getX();
         int y = (int) p.getY();
 
