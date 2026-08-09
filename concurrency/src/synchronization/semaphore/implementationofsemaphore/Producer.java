b8cda2dba0cb587d05bad6477fe38f7c10398020
kumar-shivraj <dev.shivraj.kumar@gmail.com>
2026-08-09 14:08:02 +0530

    synchronization/semaphore: comment down the scenario if we add additional release or forget to release then what will happen

--- concurrency/src/synchronization/semaphore/implementationofsemaphore/Producer.java
+++ concurrency/src/synchronization/semaphore/implementationofsemaphore/Producer.java
@@ -25,6 +25,17 @@ public class Producer implements Runnable {
             // it will definitely release a permit on the consumer semaphore, allowing a consumer to acquire it and consume the item
             // kind of try to consumer++
             consumerSemaphore.release();
+            // what will happen if we forget to release the consumerSemaphore here?
+            // If we forget to release the consumerSemaphore, consumers may be blocked indefinitely, waiting for an item to become available.
+            // situation of deadlock may occur where producers are producing items but consumers are not able to consume them because they are blocked waiting for a permit from the consumerSemaphore.
+
+
+            // what will happen if we release the consumerSemaphore again here?
+            // It will allow a consumer to acquire it and consume the item
+            // but it may lead to an imbalance between production and consumption
+            // This could result in Index -1 out of bounds for length 0 error when a consumer tries to consume an item that doesn't exist, or it could lead to a situation where producers are blocked because the store is full, even though there are consumers waiting to consume items.
+            // Therefore, it's important to release the consumerSemaphore only once after producing an item to maintain the balance between production and consumption.
+//             consumerSemaphore.release();
         }
     }
 
