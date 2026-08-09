27f71f5bf2d98a2c4809337ae1216156a7578b61
kumar-shivraj <dev.shivraj.kumar@gmail.com>
2026-08-10 04:01:51 +0530

    generics: comment down for why we don't use generic premitive and use generic class

--- generics/src/generics/implementation_with_generics/Client.java
+++ generics/src/generics/implementation_with_generics/Client.java
@@ -2,7 +2,14 @@ package generics.implementation_with_generics;
 
 public class Client {
     static void main() {
-        Pair<String , Double> p = new Pair<>("Hello", 2.0);
+        Pair<String, Double> p = new Pair<>("Hello", 2.0);
+
+
+        // this will cause compile time error
+        // as we are trying to use primitive types instead of wrapper classes
+        // there is nothing called as generic primitive
+        // the topic name is always generic Class
+        // Pair<String, double> p1 = new Pair<>();
 
         p.setX("Hello");
         p.setY(20.0);
