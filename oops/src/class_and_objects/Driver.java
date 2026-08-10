365ef40e153d7824d3e5b440ee3e2bca7d7ee4f3
kumar-shivraj <dev.shivraj.kumar@gmail.com>
2026-08-11 00:22:22 +0530

    oops: parameterized constructor in java

--- oops/src/class_and_objects/Driver.java
+++ oops/src/class_and_objects/Driver.java
@@ -31,6 +31,18 @@ public class Driver {
          */
     }
 
+    // we can also define a parameterized constructor to initialize the attributes with specific values when creating an object of the Driver class
+    // if we create a parameterized constructor, we need to explicitly define the default constructor if we want to use it as well.
+    // if we don't define the default constructor, Java will not provide it for us if we have defined a parameterized constructor and it will result in a compilation error if we try to create an object of the Driver class using the default constructor.
+    // it is because when we define a parameterized constructor, Java assumes that we don't want the default constructor anymore and it will not provide it for us.
+    // it's like : either we or java will provide the default constructor but not both. if we define a parameterized constructor, we need to explicitly define the default constructor if we want to use it as well.
+    public Driver(int id, String n, double r, boolean online) {
+        driverId = id;
+        name = n;
+        rating = r;
+        isOnline = online;
+    }
+
 
 
     // methods (behaviors) of the Driver class :
