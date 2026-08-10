c42cba394c08243d35c9f915aca81bc83346a69e
kumar-shivraj <dev.shivraj.kumar@gmail.com>
2026-08-11 01:08:20 +0530

    oops: this keyword : actual working code : resolve the ambiguity

--- oops/src/this_keyword/Driver.java
+++ oops/src/this_keyword/Driver.java
@@ -20,14 +20,27 @@ public class Driver {
         }
      */
 
-    // scenario: Using the same parameter names as the member variables
-    // ambiguity arises between the member variables and the parameters
-    // so member variables are shadowed by the parameters
-    // so member variable will not be initialized with the values passed to the constructor
+    /*
+        // scenario: Using the same parameter names as the member variables
+        // ambiguity arises between the member variables and the parameters
+        // so member variables are shadowed by the parameters
+        // so member variable will not be initialized with the values passed to the constructor
+        public Driver(int driverId, String name, double rating, boolean isOnline) {
+            driverId = driverId;
+            name = name;
+            rating = rating;
+            isOnline = isOnline;
+        }
+    */
+
+    // resolve the ambiguity using the 'this' keyword to refer to the member variables of the class
+    // 'this' keyword is used to refer to the current object of the class : calling object
+    // here we write this.driverId, similar to driver.driverId in client class
+    // so we can now interpret 'this' keyword as driver : which is calling object of the class
     public Driver(int driverId, String name, double rating, boolean isOnline) {
-        driverId = driverId;
-        name = name;
-        rating = rating;
-        isOnline = isOnline;
+        this.driverId = driverId;
+        this.name = name;
+        this.rating = rating;
+        this.isOnline = isOnline;
     }
 }
