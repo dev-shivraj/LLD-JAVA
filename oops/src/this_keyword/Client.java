669bb166d947df444ea88ba8171a846f2eb83b26
kumar-shivraj <dev.shivraj.kumar@gmail.com>
2026-08-11 01:01:10 +0530

    oops: this keyword initial setup

---
+++ oops/src/this_keyword/Client.java
@@ -0,0 +1,10 @@
+package this_keyword;
+
+public class
+Client {
+    static void main() {
+        Driver driver = new Driver(101, "John Doe", 4.8, true);
+        System.out.println("Driver ID: " + driver.driverId);
+
+    }
+}
---
+++ oops/src/this_keyword/Driver.java
@@ -0,0 +1,20 @@
+package this_keyword;
+
+public class Driver {
+
+    // member variables (attributes) of the Driver class :
+    int driverId;
+    String name;
+    double rating;
+    boolean isOnline;
+
+    public Driver() {
+    }
+
+    public Driver(int id, String n, double r, boolean online) {
+        driverId = id;
+        name = n;
+        rating = r;
+        isOnline = online;
+    }
+}
