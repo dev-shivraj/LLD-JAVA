ce1b9cae8b257f70c25f0bdc20347ae6654b2cc0
kumar-shivraj <dev.shivraj.kumar@gmail.com>
2026-08-11 01:04:19 +0530

    oops: this keyword : showing ambiguity without using this keyword

--- oops/src/this_keyword/Client.java
+++ oops/src/this_keyword/Client.java
@@ -5,6 +5,7 @@ Client {
     static void main() {
         Driver driver = new Driver(101, "John Doe", 4.8, true);
         System.out.println("Driver ID: " + driver.driverId);
+        System.out.println("DEBUG");
 
     }
 }
--- oops/src/this_keyword/Driver.java
+++ oops/src/this_keyword/Driver.java
@@ -11,10 +11,23 @@ public class Driver {
     public Driver() {
     }
 
-    public Driver(int id, String n, double r, boolean online) {
-        driverId = id;
-        name = n;
-        rating = r;
-        isOnline = online;
+    /*
+        public Driver(int id, String n, double r, boolean online) {
+            driverId = id;
+            name = n;
+            rating = r;
+            isOnline = online;
+        }
+     */
+
+    // scenario: Using the same parameter names as the member variables
+    // ambiguity arises between the member variables and the parameters
+    // so member variables are shadowed by the parameters
+    // so member variable will not be initialized with the values passed to the constructor
+    public Driver(int driverId, String name, double rating, boolean isOnline) {
+        driverId = driverId;
+        name = name;
+        rating = rating;
+        isOnline = isOnline;
     }
 }
