a6e90ca70445f9c18a66320bf619381e400b02da
kumar-shivraj <dev.shivraj.kumar@gmail.com>
2026-08-11 00:18:51 +0530

    oops: introduction to constructor

--- oops/src/class_and_objects/Driver.java
+++ oops/src/class_and_objects/Driver.java
@@ -11,6 +11,28 @@ public class Driver {
     // state of an object : the values of its attributes at a given point in time
 
 
+    // constructor of the Driver class :
+    public Driver() {
+        // default constructor : even if we don't define it, Java provides a default constructor
+        // java write it likes below ;
+        /*
+            driverId = 0;
+            name = null;
+            rating = 0.0;
+            isOnline = false;
+         */
+
+        // we can also initialize the attributes with some default values in the constructor
+        /*
+            driverId = 101;
+            name = "John Doe";
+            rating = 5.0;
+            isOnline = false;
+         */
+    }
+
+
+
     // methods (behaviors) of the Driver class :
 
     public void acceptRide(String rideId) {
