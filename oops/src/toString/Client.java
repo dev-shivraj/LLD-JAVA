3b678bccb82aa58691585da9ce9d7938256f7a31
kumar-shivraj <dev.shivraj.kumar@gmail.com>
2026-08-11 12:10:53 +0530

    oops: toString method: initial setup

---
+++ oops/src/toString/Client.java
@@ -0,0 +1,9 @@
+package toString;
+
+public class
+Client {
+    static void main() {
+        Driver driver = new Driver(1, "John Doe", 4.5, true);
+        System.out.println(driver);
+    }
+}
---
+++ oops/src/toString/Driver.java
@@ -0,0 +1,19 @@
+package toString;
+
+public class Driver {
+
+    int driverId;
+    String name;
+    double rating;
+    boolean isOnline;
+
+    public Driver() {
+    }
+
+    public Driver(int driverId, String name, double rating, boolean isOnline) {
+        this.driverId = driverId;
+        this.name = name;
+        this.rating = rating;
+        this.isOnline = isOnline;
+    }
+}
