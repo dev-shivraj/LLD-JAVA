262bd723e90a7d2c0bff5d8b314db2792e3a1232
kumar-shivraj <dev.shivraj.kumar@gmail.com>
2026-08-11 11:56:41 +0530

    oops: static block implementation

--- oops/src/static_keyword/Driver.java
+++ oops/src/static_keyword/Driver.java
@@ -22,6 +22,36 @@ public class Driver {
     // because they belong to the class rather than any specific instance
     static int totalDrivers;
 
+    // ==========   instance block  ==========
+    // static block is executed only once when the class is loaded into memory
+    // it is used to initialize static variables or perform any other static initialization
+    // it is executed before the constructor and before any instance of the class is created
+    // it is executed only once when the class is loaded into memory
+    // we can not access instance variables or instance methods in a static block as they do not belong to any specific instance and they are not yet created when the static block is executed
+    // it is recommended to use static block for static initialization only
+    // we can have multiple static blocks in a class and they will be executed in the order they are defined in the class
+    static {
+        System.out.println("------------------------------------------------");
+        System.out.println("Static block executed");
+        System.out.println("Static block is executed only once when the class is loaded into memory");
+        totalDrivers = 0;
+        System.out.println("------------------------------------------------");
+    }
+
+    static {
+        System.out.println("------------------------------------------------");
+        System.out.println("Another static block executed");
+        System.out.println("------------------------------------------------");
+    }
+    /*
+        we can also have static blocks in interfaces and they will be executed when the interface is loaded into memory
+        we can also have static blocks in abstract classes and they will be executed when the abstract class is loaded into memory
+        we can also have static blocks in enums and they will be executed when the enum is loaded into memory
+        we can also have static blocks in inner classes and they will be executed when the inner class is loaded into memory
+        we can also have static blocks in anonymous classes and they will be executed when the anonymous class is loaded into memory
+        we can also have static blocks in local classes and they will be executed when the local class is loaded into memory
+    */
+
     // ==========   static methods  ==========
     // static methods can only access static variables and other static methods
     // they cannot access instance variables or instance methods
