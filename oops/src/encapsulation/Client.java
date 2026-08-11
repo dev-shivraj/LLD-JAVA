1013411e91b148fbcb81987d04941bc49c2234e7
kumar-shivraj <dev.shivraj.kumar@gmail.com>
2026-08-12 00:02:34 +0530

    oops: encapsulation: demonstrate how client can directly modify the data if we don't protect them

--- oops/src/encapsulation/Client.java
+++ oops/src/encapsulation/Client.java
@@ -3,5 +3,11 @@ package encapsulation;
 public class Client {
     static void main() {
         Driver driver = new Driver(1, "John Doe", 4.5, true);
+
+        // Direct access to the rating field
+        // This is not recommended as any client can modify the data member without any validation or control.
+        driver.rating = 4.8;
+
+
     }
 }
