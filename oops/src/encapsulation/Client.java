0e17d9f8a34982afab0c6e741751fdcd8671590c
kumar-shivraj <dev.shivraj.kumar@gmail.com>
2026-08-12 00:05:24 +0530

    oops: encapsulation: protect the data and expose the setter in order to client can not directly access the data

--- oops/src/encapsulation/Client.java
+++ oops/src/encapsulation/Client.java
@@ -1,12 +1,18 @@
 package encapsulation;
 
 public class Client {
-    static void main() {
+    public static void main(String[] args) {
         Driver driver = new Driver(1, "John Doe", 4.5, true);
 
-        // Direct access to the rating field
-        // This is not recommended as any client can modify the data member without any validation or control.
-        driver.rating = 4.8;
+        /*
+            Direct access to the rating field
+            This is not recommended as any client can modify the data member without any validation or control.
+            driver.rating = 4.8;
+         */
+
+        // This is recommended as client can now only modify the rating field through the setter method
+        // this way we can't directly modify the rating field
+        driver.setRating(4.8);
 
 
     }
--- oops/src/encapsulation/Driver.java
+++ oops/src/encapsulation/Driver.java
@@ -2,10 +2,19 @@ package encapsulation;
 
 public class Driver {
 
-    int driverId;
-    String name;
-    double rating;
-    boolean isOnline;
+    /*
+        because of any client can modifiy the data members directly
+        we make the data members private so that they cannot be accessed directly from outside the class.
+        so we make the data members private and provide public getter and setter methods to access and modify the data members.
+        This way we can control the access to the data members and can add validation or any other logic in the getter and setter methods.
+        we can do it by access modifiers.
+        we can use private access modifier to make the data members private
+        and public access modifier to make the getter and setter methods public.
+     */
+    private int driverId;
+    private String name;
+    private double rating;
+    private boolean isOnline;
 
     public Driver() {
     }
@@ -15,5 +24,9 @@ public class Driver {
         this.name = name;
         this.rating = rating;
         this.isOnline = isOnline;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
     }
 }
