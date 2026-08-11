a37b83fa1d9c9cfa38867290e3a8489013a724a4
kumar-shivraj <dev.shivraj.kumar@gmail.com>
2026-08-11 21:21:56 +0530

    oops: demonstrate pass by value in java

--- oops/src/passByReferencePassByValue/Client.java
+++ oops/src/passByReferencePassByValue/Client.java
@@ -4,5 +4,16 @@ public class
 Client {
     public static void main(String[] args) {
 
+        int x = 10;
+
+        // it will not change the value of x because it is passed by value
+        // so add method will create a copy of x and change the value of that copy, but not the original x
+        // so the value of x will remain 10 after the method call
+        add(x);
+        System.out.println(x);
+    }
+
+    public static void add(int x) {
+        x = x + 10;
     }
 }
