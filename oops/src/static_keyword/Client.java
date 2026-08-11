a272f9ed2ed934cabd089548f4a86adf4d304b7e
kumar-shivraj <dev.shivraj.kumar@gmail.com>
2026-08-11 11:40:25 +0530

    oops: static keyword implementation

--- oops/src/static_keyword/Client.java
+++ oops/src/static_keyword/Client.java
@@ -8,7 +8,17 @@ Client {
         Driver driver3 = new Driver(103, "Mike Johnson", 4.9, true);
         Driver driver4 = new Driver(104, "Emily Davis", 4.7, false);
 
-        System.out.println("Total Drivers: " + driver2.totalDrivers);
+        /*
+            // we can use driver1.totalDrivers or driver2.totalDrivers or driver3.totalDrivers or driver4.totalDrivers to access the static variable
+            // but it is not recommended as it can be confusing
+            // it is recommended to use the class name to access the static variable
+            System.out.println("Total Drivers: " + driver2.totalDrivers);
+         */
+
+        // we use the class name to access the static variable
+        // this is the recommended way to access static variables
+        System.out.println("Total Drivers: " + Driver.totalDrivers);
+
         System.out.println("DEBUG");
 
     }
--- oops/src/static_keyword/Driver.java
+++ oops/src/static_keyword/Driver.java
@@ -7,11 +7,21 @@ public class Driver {
     double rating;
     boolean isOnline;
 
-    // if we do keep the count of total drivers in this variable
-    // then it will not work as it is not shared across all instances of the class.
-    // Each instance will have its own copy of totalDrivers
-    // and it will not reflect the total number of drivers created.
-    int totalDrivers;
+    /*
+        // if we do keep the count of total drivers in this variable
+        // then it will not work as it is not shared across all instances of the class.
+        // Each instance will have its own copy of totalDrivers
+        // and it will not reflect the total number of drivers created.
+        int totalDrivers;
+    */
+
+    // static variable to keep track of total drivers
+    // static variables are shared across all instances of the class
+    // and it will reflect the total number of drivers created.
+    // static variables are also called class variables
+    // because they belong to the class rather than any specific instance
+    static int totalDrivers;
+
 
     public Driver() {
     }
@@ -22,6 +32,21 @@ public class Driver {
         this.rating = rating;
         this.isOnline = isOnline;
 
-        this.totalDrivers++;
+        /*
+            // we can use 'this' keyword as well to refer to the static variable
+            // but it is not recommended as it can be confusing
+            this.totalDrivers++;
+         */
+
+
+        /*
+            // we use the class name to refer to the static variable
+            // this is the recommended way to refer to static variables
+            Driver.totalDrivers++;
+         */
+
+        // or if we are in the same class we can use the variable name directly
+        // this is also the recommended way to refer to static variables
+         totalDrivers++;
     }
 }
