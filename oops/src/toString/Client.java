edd101f33559c436224c80dfafa023f4bba62811
kumar-shivraj <dev.shivraj.kumar@gmail.com>
2026-08-11 21:11:51 +0530

    oops: toString method implementation

--- oops/src/toString/Client.java
+++ oops/src/toString/Client.java
@@ -2,8 +2,15 @@ package toString;
 
 public class
 Client {
-    static void main() {
+    public static void main(String[] args) {
         Driver driver = new Driver(1, "John Doe", 4.5, true);
+
+        // will print  : Driver{driverId=1, name='John Doe', rating=4.5, isOnline=true}
+        // as the toString method is overridden in the Driver class, it will be called when the object is printed.
         System.out.println(driver);
+
+        Driver driver2 = new Driver(2, "Jane Smith", 4.8, false);
+        // will print  : Driver{driverId=2, name='Jane Smith', rating=4.8, isOnline=false}
+        System.out.println(driver2);
     }
 }
--- oops/src/toString/Driver.java
+++ oops/src/toString/Driver.java
@@ -16,4 +16,24 @@ public class Driver {
         this.rating = rating;
         this.isOnline = isOnline;
     }
+
+    /**
+     * toString method is overridden to provide a string representation of the Driver object.
+     * It returns a string that includes the class name and the values of its fields in a readable format.
+     * This is useful for debugging and logging purposes, as it allows developers to easily see the
+     * state of a Driver object when it is printed or logged.
+     */
+    @Override
+    public String toString() {
+
+        // returns hello for every object of Driver class
+        // return "hello";
+
+        return "Driver{" +
+                "driverId=" + driverId +
+                ", name='" + name + '\'' +
+                ", rating=" + rating +
+                ", isOnline=" + isOnline +
+                '}';
+    }
 }
