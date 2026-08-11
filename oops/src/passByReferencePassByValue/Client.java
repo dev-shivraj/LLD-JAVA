45f5f1c57e849c066a06cc8414c32ae5002071b0
kumar-shivraj <dev.shivraj.kumar@gmail.com>
2026-08-11 21:14:03 +0530

    oops: pass by value and paas by reference : initial setup

---
+++ oops/src/passByReferencePassByValue/Client.java
@@ -0,0 +1,8 @@
+package passByReferencePassByValue;
+
+public class
+Client {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ oops/src/passByReferencePassByValue/Driver.java
@@ -0,0 +1,20 @@
+package passByReferencePassByValue;
+
+public class Driver {
+
+    int driverId;
+    String name;
+    double rating;
+    boolean isOnline;
+
+    public Driver() {
+    }
+
+    public Driver(int driverId, String name, double rating, boolean isOnline) {
+        this.driverId = driverId;
+        this.name = name;
+        this.rating = rating;
+        this.isOnline = isOnline;
+    }
+
+}
