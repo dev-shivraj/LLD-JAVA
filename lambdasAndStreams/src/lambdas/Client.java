24182fee0079770c6aa80403a70014302f408d8b
kumar-shivraj <dev.shivraj.kumar@gmail.com>
2026-08-13 22:50:10 +0530

    lambdas and streams: creating a basic lambda

--- lambdasAndStreams/src/lambdas/Client.java
+++ lambdasAndStreams/src/lambdas/Client.java
@@ -26,8 +26,37 @@ package lambdas;
  */
 public class Client {
     public static void main(String[] args) {
-        Task task = new Task();
+
+        /*
+            Task task = new Task();
+            Thread thread = new Thread(task);
+            thread.start();
+         */
+
+        /*
+            here printing something inside run method is not a big task,
+            and we are going to use it only once or similar kind of things at certain place, so
+            just creating a separate class for it is not a good idea, so
+            we can use lambda expression to implement the run method of Runnable interface,
+            and we can pass it to the Thread constructor, so that we don't have to create a separate class for it.
+
+            so if the task is simple, and we feel that if we are going to use it only one or two places
+            then we can use lambda expression to implement the same and avoid creating a separate class for it.
+         */
+
+
+        Runnable task = () -> {
+            /*
+                here bracket represents the body of the run method,
+                and we can write the code inside it, which will be executed when the thread is started.
+                and Runnable represents the Class name here.
+                here function doesn't accept any parameter of run method, so we don't have to pass any parameter to it.
+             */
+            System.out.println("Task is running");
+        };
+
         Thread thread = new Thread(task);
         thread.start();
+
     }
 }
