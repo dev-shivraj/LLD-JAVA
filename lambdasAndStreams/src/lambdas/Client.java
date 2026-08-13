a885c9fc5e4cc9f79babb9b8642ae48ccb249452
kumar-shivraj <dev.shivraj.kumar@gmail.com>
2026-08-13 22:55:09 +0530

    lambdas and streams: some points about lambda and functional interface

--- lambdasAndStreams/src/lambdas/Client.java
+++ lambdasAndStreams/src/lambdas/Client.java
@@ -58,5 +58,24 @@ public class Client {
         Thread thread = new Thread(task);
         thread.start();
 
+        /*
+            why lambda works on functional interface only?
+            because functional interface has only one abstract method,
+            so the compiler can infer the type of the lambda expression based on the context in which it is used.
+
+            and if the interface has more than one abstract method
+            then the compiler cannot be sure that which function of the interface is being implemented by the lambda expression,
+            so it will throw an error, and we cannot use lambda expression to implement the interface with more than one abstract method.
+         */
+
+
+        /*
+            where to use lambda expression?
+            1. when we have a simple onetime case throwaway implementation of a functional interface with a single abstract method, we can use lambda expression to implement it.
+            2. when we have to pass a block of code as an argument to a method
+            3. when we have to create an anonymous class with a single abstract method.
+         }
+
+
     }
 }
