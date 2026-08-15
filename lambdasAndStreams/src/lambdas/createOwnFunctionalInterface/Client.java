a06f23f8496eafdcb2ef1abc04b766b87e134946
kumar-shivraj <dev.shivraj.kumar@gmail.com>
2026-08-15 14:18:28 +0530

    lambdas and streams: use lambdas to use our own created functional interface

--- lambdasAndStreams/src/lambdas/createOwnFunctionalInterface/Client.java
+++ lambdasAndStreams/src/lambdas/createOwnFunctionalInterface/Client.java
@@ -5,5 +5,11 @@ public class Client {
         AdderWithoutLambdas adderWithoutLambdas = new AdderWithoutLambdas();
         int result = adderWithoutLambdas.calculate(1, 2, 3);
         System.out.println("Result without lambdas: " + result);
+
+
+        // using lambda expression to implement the same functionality
+        Calculator adderWithLambdas = (a, b, c) -> a + b + c;
+        int resultWithLambdas = adderWithLambdas.calculate(1, 2, 3);
+        System.out.println("Result with lambdas: " + resultWithLambdas);
     }
 }
