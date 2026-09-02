006d98bd451f06bcf391149bbaeaa40326966207
kumar-shivraj <dev.shivraj.kumar@gmail.com>
2026-09-02 08:55:50 +0530

    design patterns: builder: add the code of basic builder understanding

---
+++ designPatterns/src/practice/builder/InstructorClient.java
@@ -0,0 +1,18 @@
+package practice.builder;
+
+public class InstructorClient {
+
+
+    // Create an Instructor class with name, email, salary, age, company attributes.
+    // instructor shluld only be created if he is working professional.
+    // Builder
+    public static void main(String[] args) {
+        Instructor instructor = Instructor.getBuilder()
+                .setAge(30)
+                .setEmail("jnask")
+                .setName("Virat")
+                .setCompany("jdsakd")
+                .build();
+    }
+
+}
