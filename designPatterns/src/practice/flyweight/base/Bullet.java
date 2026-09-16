623fe46ac81fb7e9583207d9d1fe1c345beb63b4
kumar-shivraj <dev.shivraj.kumar@gmail.com>
2026-09-16 09:07:21 +0530

    design patterns: flyweight: add basic codes to understand the decorator flyweight pattern

---
+++ designPatterns/src/practice/flyweight/base/Bullet.java
@@ -0,0 +1,14 @@
+package practice.flyweight.base;
+
+public class Bullet {
+
+    private BulletType bulletType;
+    private int x;
+    private int y;
+
+    public Bullet(String bulletName, int x, int y) {
+        this.bulletType = BulletTypeFactory.getBulletType(bulletName);
+        this.x = x;
+        this.y = y;
+    }
+}
